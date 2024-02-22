5bb37d11d2d46e1bf49e37f358d15ff58de8982d
abhir <abhiramashok1062004@gmail.com>
2024-02-22 18:44:33 +0530

    Initial Commit

--- Source_Code.py
+++ Source_Code.py
@@ -16,12 +16,12 @@ Plr_batting = 0
 
 
 def buttons_clicked(txt):
-    global score2,lb22, lb25,lb, Sys_batting, Plr_batting
+    global score2, lb22, lb25, lb, Sys_batting, Plr_batting
     lb.place_forget()
     if Plr_batting:
         score2 += int(txt)
     lb25.config(text=txt)
-    print(txt,end=" ")
+    print(txt, end=" ")
     play(txt)
 
 
@@ -30,7 +30,7 @@ def play(txt):
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     random_num = random.choice(numbers)
     lb14.config(text=random_num)
-    num=int(txt)
+    num = int(txt)
     if Sys_batting == 1:
         score1 += random_num
     if num == random_num:
@@ -56,13 +56,13 @@ def play(txt):
             lb12.config(text="Score: " + str(score1))
 
     if (score1 > 0) and (score2 > 0):
-        if (Sys_batting==1) and (score1 > score2):
+        if (Sys_batting == 1) and (score1 > score2):
             print("GAME OVERR")
             lb.place_forget()
             frame4.place(relx=0.5, rely=0.53, anchor=tk.CENTER, width=700, height=700)
             lb.config(text="System Won")
             return
-        elif (Plr_batting==1) and (score1 < score2):
+        elif (Plr_batting == 1) and (score1 < score2):
             print("GAME OVERR")
             lb.place_forget()
             frame4.place(relx=0.5, rely=0.53, anchor=tk.CENTER, width=700, height=700)
@@ -99,7 +99,7 @@ def reset():
 
 
 def setall():
-    global lb25,lb14,Sys_batting,Plr_batting
+    global lb25, lb14, Sys_batting, Plr_batting
     if random.choice([1, 2]) == 1:
         Sys_batting = 1
         lb24.config(text=" You are Bowling🏐 ")
@@ -111,7 +111,7 @@ def setall():
 
 
 def restart():
-    global frame4,score1,score2,Sys_batting,Plr_batting,highscore,lb12,lb22,lb
+    global frame4, score1, score2, Sys_batting, Plr_batting, highscore, lb12, lb22, lb
     frame4.place_forget()
     if score2 > highscore:
         highscore = score2
@@ -122,32 +122,28 @@ def restart():
     reset()
 
 
-
 # Creating Frames
-# frame = tk.Frame(window)
-# frame.pack(fill=tk.BOTH, expand=True,padx=10,pady=10)
-
 frame1 = tk.Frame(window, bg="#00FFFF")
 frame1.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-lb1 = tk.Label(frame1, text="SYSTEM", bg="#00FFFF", fg="#040406", font=("Arial",22,"bold","underline"))
+lb1 = tk.Label(frame1, text="SYSTEM", bg="#00FFFF", fg="#040406", font=("Arial", 22, "bold", "underline"))
 lb1.pack(side=tk.TOP, expand=False, pady=10)
-lb11 = tk.Label(frame1, text="", bg="#00FFFF", font=("Arial",15,""))
+lb11 = tk.Label(frame1, text="", bg="#00FFFF", font=("Arial", 15, ""))
 lb11.pack()
 lb12 = tk.Label(frame1, text="Score: "+"0", bg="#00FFFF", font=("Arial", 18, "bold"))
 lb12.pack()
 lb13 = tk.Label(frame1, text="System is Bowling🏐", bg="#00FFFF", font=("Arial", 15, "bold"))
 lb13.pack(side=tk.BOTTOM, pady=10)
-lb14 = tk.Label(frame1,text="", bg="#00FFFF", font=("Arial", 25, "bold"))
-lb14.place(relx=0.5,rely=0.8, anchor=tk.CENTER)
-img= tk.PhotoImage(file="img.png")
-lb15 = tk.Label(image=img,width=250, height=250,bg="#00FFFF")
-lb15.place(relx=0.25,rely=0.52,anchor=tk.CENTER)
+lb14 = tk.Label(frame1, text="", bg="#00FFFF", font=("Arial", 25, "bold"))
+lb14.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+img = tk.PhotoImage(file="img.png")
+lb15 = tk.Label(image=img, width=250, height=250, bg="#00FFFF")
+lb15.place(relx=0.25, rely=0.52, anchor=tk.CENTER)
 
 frame2 = tk.Frame(window, bg="#FC46AA")
 frame2.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-lb2 = tk.Label(frame2,text="PLAYER",bg="#FC46AA",fg="white",font=("Arial",22,"bold","underline"))
+lb2 = tk.Label(frame2, text="PLAYER", bg="#FC46AA", fg="white", font=("Arial", 22, "bold", "underline"))
 lb2.pack(side=tk.TOP, expand=False, pady=10)
-lb21 = tk.Label(frame2, text="", bg="#FC46AA", font=("Arial",15,""))
+lb21 = tk.Label(frame2, text="", bg="#FC46AA", font=("Arial", 15, ""))
 lb21.pack()
 lb22 = tk.Label(frame2, text="Score: "+"5", bg="#FC46AA", font=("Arial", 18, "bold"))
 lb22.pack()
@@ -156,43 +152,37 @@ lb23 = tk.Label(frame2, text="Click any of the button", bg="#FC46AA", font=("Ari
 lb23.place(relx=0.5, rely=0.4, anchor=tk.S)
 lb24 = tk.Label(frame2, text=" You are Batting🦇 ", bg="#FC46AA", font=("Arial", 15, "bold"))
 lb24.pack(side=tk.BOTTOM, pady=10)
-lb25 = tk.Label(frame2,text="", bg="#FC46AA", font=("Arial", 25, "bold"))
-lb25.place(relx=0.5,rely=0.8, anchor=tk.CENTER)
+lb25 = tk.Label(frame2, text="", bg="#FC46AA", font=("Arial", 25, "bold"))
+lb25.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
 frame3 = tk.Frame(frame2, bg="#FC46AA")
-frame3.pack(side=tk.TOP, fill=tk.X, expand=True)  # Expand horizontally to fill available space
+frame3.pack(side=tk.TOP, fill=tk.X, expand=True)
 frame3.place(relx=0.5, rely=0.53, anchor=tk.CENTER)
 b = []
 j = 1
 k = 0
 for i in range(10):
     b.append(tk.Button(frame3, text=i+1, command=lambda num=i+1: buttons_clicked(num),
-               width=3, height=2,  # Set width and height of the button
-                fg="black" ,bg ="#FEC5E5",# Set background and foreground (text) colors
-               font=("Arial", 12,"bold"),  # Set font family and size
-               relief=tk.RAISED,  # Set relief style (BORDER, FLAT, RAISED, SUNKEN)
-               borderwidth=4,  # Set border width
-               padx=3, pady=5) )
+             width=3, height=2, fg="black", bg="#FEC5E5", font=("Arial", 12, "bold"),
+             relief=tk.RAISED, borderwidth=4, padx=2, pady=2))
     b[i].grid(row=j, column=k)
-    k=k+1
-    if i==4:
-        j=2
-        k=0
+    k = k+1
+    if i == 4:
+        j = 2
+        k = 0
 
-frame4=tk.Frame(window, bg="black")
-lb41=tk.Label(frame4,text="GAME OVER !\n Click RESTART to continue", font=("Arial",20,"bold"),bg="black",fg="white")
+frame4 = tk.Frame(window, bg="black")
+lb41 = tk.Label(frame4, text="GAME OVER !\n Click RESTART to continue", font=("Arial", 20, "bold"), bg="black",
+                fg="white")
 lb41.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
-b41=tk.Button(frame4,text="RESTART", font=("Arial",15,"bold"),bg="#4CBB17",fg="black",command=lambda:reset())
+b41 = tk.Button(frame4, text="RESTART", font=("Arial", 15, "bold"), bg="#4CBB17", fg="black", command=lambda: reset())
 b41.place(relx=0.5, rely=0.51, anchor=tk.CENTER)
-#frame4.place(relx=0.5, rely=0.53, anchor=tk.CENTER, width=600, height=600)
 
 
-button = tk.Button(window, text="RESET", width=7, height=1, font=("Courier",12,"bold"), fg="black", command=lambda: restart())
+button = tk.Button(window, text="RESET", width=7, height=1, font=("Courier", 12, "bold"), fg="black",
+                   command=lambda: restart())
 button.pack(side=tk.TOP, fill=tk.X, expand=True)  # Expand horizontally to fill available space
 button.place(relx=0.5, rely=0.21, anchor=tk.CENTER)
-lb =tk.Label(window,text="OUT !!!",width=8, height=1, font=("Courier",20,"bold"), fg="white",bg="red")
-#lb.place(relx=0.5, rely=0.821, anchor=tk.CENTER)
+lb = tk.Label(window, text="OUT !!!", width=8, height=1, font=("Courier", 20, "bold"), fg="white", bg="red")
 setall()
 window.mainloop()
-
-
